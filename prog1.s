4befd7a60b170f5baef7796be1c9223c3bc1d4ef
Arindaam <42868650+Arindaam@users.noreply.github.com>
2020-01-20 12:26:10 +0530

    Add files via upload

---
+++ prog1.s
@@ -0,0 +1,10 @@
+.text
+        MOV r0,#10
+        MOV r1,#1
+        ADD r2,r0,r1
+        SUB r3,r2,r1
+        AND r4,r1,#0
+        ORR r5,r1,#0
+        EOR r6,r1,#0   
+        AND r7,r0,#1
+.end
